2a104abb943ea3ca9390f19128460fb5bb5ddbb9
suyin-long <suyin_long@163.com>
2021-01-29 14:25:42 +0800

    定制候选人列表的筛选、查找、排序功能

--- interview/admin.py
+++ interview/admin.py
@@ -4,10 +4,18 @@ from interview.models import Candidate
 
 
 # Register your models here.
+# 候选人管理类
 class CandidateAdmin(admin.ModelAdmin):
     exclude = ['creator', 'created_date', 'modified_date']
     list_display = ['username', 'city', 'bachelor_school', 'first_score', 'first_result', 'first_interviewer_user',
                     'second_result', 'second_interviewer_user', 'hr_score', 'hr_result', 'last_editor']
+    # 筛选条件
+    list_filter = ['city', 'first_result', 'second_result', 'hr_result', 'first_interviewer_user', 'second_interviewer_user', 'hr_interviewer_user']
+    # 查询字段
+    search_fields = ['username', 'phone', 'email', 'bachelor_school',]
+    # 排序字段
+    ordering = ['hr_result', 'second_result', 'first_result']
+    # 分组展示字段， 分三块：基础信息、第一轮面试记录、第二轮面试（专业复试）、HR复试
     fieldsets = [
         ('基本信息', {'fields': ['userid', ('username', 'city', 'phone'), ('email', 'apply_position', 'born_address'), ('gender', 'candidate_remark'), ('bachelor_school', 'master_school', 'doctor_school'), ('major', 'degree'),  ('test_score_of_general_ability', 'paper_score'), 'last_editor',
 ]}),
