941db1f86447b846dcc1221125857263dec8f394
suyin-long <suyin_long@163.com>
2021-02-07 22:34:30 +0800

    28更新

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
+from interview.models import Candidate
+from django.contrib import messages
+from datetime import datetime
+
 from jobs.models import Job
 from jobs.models import Resume
 
@@ -14,6 +18,21 @@ class JobAdmin(admin.ModelAdmin):
         obj.creator = request.user
         super().save_model(request, obj, form, change)
 
+def enter_interview_process(modeladmin, request, queryset):
+    candidate_names = ""
+    for resume in queryset:
+        candidate = Candidate()
+        # 把 obj 对象中的所有属性拷贝到 candidate 对象中:
+        candidate.__dict__.update(resume.__dict__)
+        candidate.created_date = datetime.now()
+        candidate.modified_date = datetime.now()
+        candidate_names = candidate.username + "," + candidate_names
+        candidate.creator = request.user.username
+        candidate.save()
+    messages.add_message(request, messages.INFO, '候选人: %s 已成功进入面试流程' % (candidate_names) )
+
+
+enter_interview_process.short_description = u"进入面试流程"
 
 class ResumeAdmin(admin.ModelAdmin):
     # actions = (enter_interview_process,)
