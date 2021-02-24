3cb5c8f999557830b303f0d9d1d91a5290a8df3b
suyin-long <suyin_long@163.com>
2021-02-25 00:46:50 +0800

    单元测试

--- jobs/urls.py
+++ jobs/urls.py
@@ -35,7 +35,3 @@ urlpatterns = [
 #     urlpatterns = [
 #                       path('__debug__/', include(debug_toolbar.urls)),
 #                   ] + urlpatterns
-
-if settings.DEBUG:
-    # 有 XSS 漏洞的视图页面，
-    urlpatterns += [url(r'^detail_resume/(?P<resume_id>\d+)/$', views.detail_resume, name='detail_resume'), ]
